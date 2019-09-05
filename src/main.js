3059099d714a82ecff9052dcc233128a0d577a23
Dwayne McFee <dwayne@merlinsoftware.com.au>
2019-09-05 10:46:08 +0930

    Sitepoint Matching Game Tutorial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+
+import _ from 'lodash'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  _,
+  render: h => h(App)
 }).$mount('#app')
